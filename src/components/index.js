6b7951067b11f3a23c13c510fb9b6d938bf28932
Dzun <nurroinz@gmail.com>
2022-10-03 22:22:55 +0700

    slicing : Stats & Business Section #4

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import Business from "./Business";
+import Stats from "./Stats";
 
 export {
   Navbar,
   Hero,
+  Business,
+  Stats,
 };
